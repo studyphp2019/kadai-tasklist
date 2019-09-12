264cdc516c9ae588ea957e4f7b5c2006d3ae2e55
franti <ec2-user@ip-172-31-42-41.us-east-2.compute.internal>
2019-09-12 08:31:47 +0000

    tasks CRUD

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>課題用タスク管理アプリ</title>
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+    <style type="text/css">
+        .wrap {
+            max-width: 1000px;
+            margin: 100px auto 0;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrap">
+            @yield('content')
+        
+    </div>
+            
+            
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+        <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js"></script>
+</body>
+</html>
---
+++ resources/views/tasks/edit.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="row">
+        <div class="col-6">
+            {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+        
+                <div class="form-group">
+                    {!! Form::label('content', 'メッセージ:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+        
+                {!! Form::submit('更新', ['class' => 'btn btn-light']) !!}
+        
+            {!! Form::close() !!}
+        </div>
+    </div>
+
+@endsection
